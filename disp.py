157daf7c367fcd5bec82b97db971f87d9ccce300
Rui Xu <ruix@princeton.edu>
2018-01-23 17:39:24 -0500

    add parallel propagation

--- disp.py
+++ disp.py
@@ -14,48 +14,60 @@ def det(u0,*arg):
   logger= logging.getLogger(__name__)
   theta = p['theta'][0]*np.pi/180.
   omega = u0[0]+1j*u0[1]
-    
   D     = np.zeros((3,3),dtype=complex)
-  for i in range(int(p['Nsp'][0])):
-    al    = (wave_k*np.sin(theta))**2/2.0/p['Omega'][i]**2*\
-            p['bperp'][i]/p['bperp'][0]*p['mu'][i]
-    delta = p['bperp'][i]/p['bpara'][i]
-    kVOmp = wave_k*np.cos(theta)*p['V'][i]/np.sqrt(p['bperp'][0])
-    kpwp  = wave_k*np.cos(theta)*np.sqrt(p['bpara'][i]/p['bperp'][0]*p['mu'][i])
-    pref  = p['mu'][i]*p['den'][i]/omega
-    
-    D[2,2]+= pref*2*p['V'][i]/wave_k/np.cos(theta)*\
-             np.sqrt(p['bperp'][0]/p['mu'][i])/p['bperp'][i]*\
-             np.sqrt(p['den'][i]/p['mu'][i])
 
-    for n in range(-int(p['m'][0]),int(p['m'][0])+1):
-      ze   = (omega-kVOmp-n*p['Omega'][i])/kpwp
-      An   = (delta-1)/omega+((omega-kVOmp-n*p['Omega'][i])*delta\
-             + n*p['Omega'][i])/kpwp/omega*f.Z(ze)
-      Bn   = 1.-kVOmp/omega + (omega-n*p['Omega'][i])*An
-
-      D[0,0]+= n**2*sp.ive(n,al)*An/al*pref
-      D[0,1]+= 1j*n*f.dive(n,al,1)*An*pref
-      D[1,0]+= -1j*n*f.dive(n,al,1)*An*pref
-      D[0,2]+= n*sp.ive(n,al)*Bn*np.tan(theta)/al/p['Omega'][i]*pref
-      D[2,0]+= n*sp.ive(n,al)*Bn*np.tan(theta)/al/p['Omega'][i]*pref
-      D[1,1]+= An*(n**2/al*sp.ive(n,al)-2*al*f.dive(n,al,1))*pref
-      D[1,2]+= -1j*f.dive(n,al,1)*Bn*np.tan(theta)/p['Omega'][i]*pref
-      D[2,1]+= 1j*f.dive(n,al,1)*Bn*np.tan(theta)/p['Omega'][i]*pref
-      D[2,2]+= 2.*sp.ive(n,al)*Bn*(omega-n*p['Omega'][i])/(wave_k**2*\
-                np.cos(theta)**2*p['bperp'][i]/p['bperp'][0])/p['mu'][i]*pref
+  if(theta>0.1): # oblique propagation
+      for i in range(int(p['Nsp'][0])):
+        al    = (wave_k*np.sin(theta))**2/2.0/p['Omega'][i]**2*\
+                p['bperp'][i]/p['bperp'][0]*p['mu'][i]
+        delta = p['bperp'][i]/p['bpara'][i]
+        kVOmp = wave_k*np.cos(theta)*p['V'][i]/np.sqrt(p['bperp'][0])
+        kpwp  = wave_k*np.cos(theta)*np.sqrt(p['bpara'][i]/p['bperp'][0]*p['mu'][i])
+        pref  = p['mu'][i]*p['den'][i]
         
-  # add (va_c term comes from displacement current)
-  rn      = wave_k/np.sqrt(p['bperp'][0])/omega
-  idty    = p['va'][0]**2
-  D[0,0] += idty - (rn*np.cos(theta))**2
-  D[0,2] += rn**2*np.cos(theta)*np.sin(theta)
-  D[1,1] += idty - rn**2
-  D[2,0] += rn**2*np.cos(theta)*np.sin(theta)
-  D[2,2] += idty - (rn*np.sin(theta))**2
-  res = LA.det(D)
-  print (D)
-  sys.exit()
+        D[2,2]+= pref*2*p['V'][i]/wave_k/np.cos(theta)*\
+                 np.sqrt(p['bperp'][0]/p['mu'][i])/p['bperp'][i]*\
+                 np.sqrt(p['den'][i]/p['mu'][i])
+
+        for n in range(-int(p['m'][0]),int(p['m'][0])+1):
+          ze   = (omega-kVOmp-n*p['Omega'][i])/kpwp
+          An   = (delta-1)/omega+((omega-kVOmp-n*p['Omega'][i])*delta\
+                 + n*p['Omega'][i])/kpwp/omega*f.Z(ze)
+          Bn   = 1.-kVOmp/omega + (omega-n*p['Omega'][i])*An
+
+          D[0,0]+= n**2*sp.ive(n,al)*An/al*pref
+          D[0,1]+= 1j*n*f.dive(n,al,1)*An*pref
+          D[1,0]+= -1j*n*f.dive(n,al,1)*An*pref
+          D[0,2]+= n*sp.ive(n,al)*Bn*np.tan(theta)/al/p['Omega'][i]*pref
+          D[2,0]+= n*sp.ive(n,al)*Bn*np.tan(theta)/al/p['Omega'][i]*pref
+          D[1,1]+= An*(n**2/al*sp.ive(n,al)-2*al*f.dive(n,al,1))*pref
+          D[1,2]+= -1j*f.dive(n,al,1)*Bn*np.tan(theta)/p['Omega'][i]*pref
+          D[2,1]+= 1j*f.dive(n,al,1)*Bn*np.tan(theta)/p['Omega'][i]*pref
+          D[2,2]+= 2.*sp.ive(n,al)*Bn*(omega-n*p['Omega'][i])/(wave_k**2*\
+                    np.cos(theta)**2*p['bperp'][i]/p['bperp'][0])/p['mu'][i]*pref
+            
+      # add (va_c term comes from displacement current)
+      rn      = wave_k/np.sqrt(p['bperp'][0]*omega)
+      idty    = p['va'][0]**2*omega
+      D[0,0] += idty - (rn*np.cos(theta))**2
+      D[0,2] += rn**2*np.cos(theta)*np.sin(theta)
+      D[1,1] += idty - rn**2
+      D[2,0] += rn**2*np.cos(theta)*np.sin(theta)
+      D[2,2] += idty - (rn*np.sin(theta))**2
+      res = LA.det(D)
+  else: # parallel propagation
+      pol = +1
+      res = p['va'][0]**2*omega**2 - wave_k**2/p['bperp'][0]
+      for i in range(int(p['Nsp'][0])):
+        delta = p['bperp'][i]/p['bpara'][i]-1.
+        kVOmp = wave_k*p['V'][i]/np.sqrt(p['bperp'][0])
+        kpwp  = wave_k*np.sqrt(p['bpara'][i]/p['bperp'][0]*p['mu'][i])
+        pref  = p['mu'][i]*p['den'][i]
+        ze    = (omega-kVOmp+pol*p['Omega'][i])/kpwp
+        ze0   = (omega-kVOmp)/kpwp
+        res  += (ze0*f.Z(ze) - delta*f.dp(ze,0)/2.)*pref
+        #res  += pref*(delta-1.+(delta*(omega-kVOmp)+pol*
+        #        (delta-1.)*p['Omega'][i]))*f.Z(ze)/kpwp
   return (res.real,res.imag)
 
 if __name__ == '__main__':
--- vis.py
+++ vis.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
+mpl.rc('font', family='sans-serif')
 
 parser = argparse.ArgumentParser(description='Specify command line arguments')
 parser.add_argument('-i','--input', help='Input file name',required=False)
@@ -13,11 +15,17 @@ if __name__ == '__main__':
     data = np.load('output.npy').item()
   else:
     data = np.load(args.input+'.npy').item()
-  plt.plot(data['wave_k'],data['fzeta'].imag,lw=2,label='Imag')
-  plt.plot(data['wave_k'],data['fzeta'].real,lw=2,label='Real')
+  x = data['wave_k']/np.sqrt(data['bperp'][0]*data['mu'][1])
+  #plt.plot(data['wave_k'],data['fzeta'].imag/abs(data['Omega'][1]),lw=2,c='r',label='Imag')
+  #plt.plot(data['wave_k'],data['fzeta'].real/abs(data['Omega'][1]),lw=2,c='k',label='Real')
+  plt.plot(data['wave_k'],data['fzeta'].imag,lw=2,c='r',label='Imag')
+  plt.plot(data['wave_k'],data['fzeta'].real,lw=2,c='k',label='Real')
+
   plt.xlabel(r'$k\rho_i$')
+  #plt.xlabel(r'$kc/\omega_{pe}$')
   plt.ylabel('$\omega/\Omega_i$')
   plt.rcParams['font.size'] = 16
+  #plt.ylim([0,6e-4])
   #plt.savefig('sample.pdf',format='pdf')
   plt.legend()
   plt.show()
